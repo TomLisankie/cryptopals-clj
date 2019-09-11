13f17754ebb5821a620b658135a60a4c91ece7e6
Thomas Lisankie <thomas@tomlisankie.com>
2019-09-11 14:32:08 -0400

    Refactored code to have better formatting and be more idiomatic according to some helpful people on Reddit

--- src/cryptopals_clj/core.clj
+++ src/cryptopals_clj/core.clj
@@ -1,19 +1,25 @@
 (ns cryptopals-clj.core)
 
-(defn hex-string->base64-string
-  "converts hex string to base64 string"
-  [original-hex-string]
+;; Challenge 1
+;; Problem 1
 
-  (let [hex-string->hex-byte-array (fn [hex-string]
-                                     (let [convert (fn [string hex-bytes]
-                                                     (if (empty? string)
-                                                       hex-bytes
-                                                       (recur (rest (rest string)) (conj hex-bytes (Long/decode (str "0x" (first string) (second string)))))))]
-                                       (byte-array (convert hex-string []))))
-        hex-byte-array->base64-byte-array (fn [hex-byte-array]
-                                            (.encode (java.util.Base64/getEncoder) hex-byte-array))
-        base64-byte-array->base64-string (fn [base64-byte-array]
-                                           (apply str (map char base64-byte-array)))]
-    (->> original-hex-string hex-string->hex-byte-array hex-byte-array->base64-byte-array base64-byte-array->base64-string)))
+(defn hex-string->byte-array
+  "converts a hex string into an array of corresponding bytes"
+  [hex-string]
+  (->> hex-string
+       (partition 2) ;; get two hex chars at a time
+       (map #(apply str "0x" %))
+       (map #(Long/decode %))
+       (byte-array)))
+
+(defn hex->base64
+  "Converts a hex string to base64"
+  [hex-string]
+  (let [encoder (java.util.Base64/getEncoder)]
+    (->> hex-string
+         (hex-string->byte-array)
+         (.encode encoder)          ;; returns a byte array
+         (map char)
+         (apply str))))
 
 
--- test/cryptopals_clj/core_test.clj
+++ test/cryptopals_clj/core_test.clj
@@ -2,8 +2,12 @@
   (:require [clojure.test :refer :all]
             [cryptopals-clj.core :refer :all]))
 
-(deftest hex-string->base64-string-test
+(deftest hex-string->byte-array-test
+  (testing "hex to byte array conversion"
+    (is (= (class (hex-string->byte-array "49276d")) (Class/forName "[B")))))
+
+(deftest hex->base64-test
   "Tests to see if hex string to base64 string conversion works"
   (testing "hex to base64 conversion"
-    (is (= (hex-string->base64-string "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+    (is (= (hex->base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"))))
