e65cef55a8f980052ced3e80c2222cb9738bb114
Thomas Lisankie <thomas@tomlisankie.com>
2019-09-11 16:00:53 -0400

    Solved challenge 2 of set 1

--- src/cryptopals_clj/core.clj
+++ src/cryptopals_clj/core.clj
@@ -1,7 +1,7 @@
 (ns cryptopals-clj.core)
 
+;; Set 1
 ;; Challenge 1
-;; Problem 1
 
 (defn hex-string->byte-array
   "converts a hex string into an array of corresponding bytes"
@@ -22,4 +22,30 @@
          (map char)
          (apply str))))
 
+;; Challenge 2
+(defn hex-string->nibble-array
+  "converts a hex string into an array of corresponding bytes"
+  [hex-string]
+  (->> hex-string
+       (partition 1)
+       (map #(apply str "0x" %))
+       (map #(Long/decode %))
+       (byte-array)))
+
+(defn fixed-xor
+  "performs a bitwise xor between two equal-length hex strings"
+  [first-string second-string]
+  (assert (= (count first-string) (count second-string)))
+  (let [resulting-bytes
+        (loop [first-array (hex-string->nibble-array first-string)
+         second-array (hex-string->nibble-array second-string)
+         result-vec []]
+          (if (empty? first-array)
+           result-vec
+           (recur (rest first-array)
+             (rest second-array)
+             (conj result-vec (bit-xor (first first-array) (first second-array))))))]
+    (->> resulting-bytes
+         (map #(Integer/toHexString %))
+         (apply str))))
 
--- test/cryptopals_clj/core_test.clj
+++ test/cryptopals_clj/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [cryptopals-clj.core :refer :all]))
 
+;; Set 1
+;; Challenge 1
 (deftest hex-string->byte-array-test
   (testing "hex to byte array conversion"
     (is (= (class (hex-string->byte-array "49276d")) (Class/forName "[B")))))
@@ -11,3 +13,9 @@
   (testing "hex to base64 conversion"
     (is (= (hex->base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"))))
+
+;; Challenge 2
+(deftest fixed-xor-test
+  "Tests to see if a fixed XOR opertation on two strings produces the proper output"
+  (testing "fixed-xor"
+    (is (= (fixed-xor "1c0111001f010100061a024b53535009181c" "686974207468652062756c6c277320657965") "746865206b696420646f6e277420706c6179"))))
